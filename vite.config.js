59b75ab72312d446a1576f69c3533f2ba205e278
Jimmy2202 <artrkmj214@gmail.com>
2025-04-25 00:44:31 -0300

    mudança no base

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/Projeto1-Avanti/",
+  base: "/Projeto2-Avanti-Github/",
   plugins: [react()],
 });
